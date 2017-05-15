67cf0b6eec072e0f8591470bc3fdac08f5fc02b7
Tomasz Kacprzak <Tomasz Kacprzak>
2017-05-15 20:33:53 +0200

    Quotes-Done

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 $(function (){
 
 	var tweetLink = "https://twitter.com/intent/tweet?text=";
-	var quoteUrl = "https://quotesondesign.com/wp-json/posts?filter[orderby]=rand&filter[posts_per_page]=1";
+	var quoteUrl = "http://quotesondesign.com/wp-json/posts?filter[orderby]=rand&filter[posts_per_page]=1";
 
 	function getQuote() {
 		$.getJSON(quoteUrl, createTweet);
