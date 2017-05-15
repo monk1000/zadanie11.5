a280bb3382b0a7bc83bc39de33b9bb7db4f15217
Tomasz Kacprzak <Tomasz Kacprzak>
2017-05-15 12:15:11 +0200

    Quotes-Done

--- script.js
+++ script.js
@@ -1,26 +1,34 @@
 var tweetLink = "https://twitter.com/intent/tweet?text=";
-var quoteUrl = "http://api.forismatic.com/api/1.0/?method=getQuote&key=867576&format=jsonp&lang=en&jsonp=?";
+var quoteUrl = "https://quotesondesign.com/wp-json/posts?filter[orderby]=rand&filter[posts_per_page]=1";
 
 function getQuote() {
 	$.getJSON(quoteUrl, createTweet);
+
+
+	function createTweet(input) {
+
+		var data = input[0];
+
+	    var quoteText = $(data.content).text().trim();
+	    var quoteAuthor = data.title;
+
+	    if (!input.quoteAuthor.lenght) {
+			input.quoteAuthor = "Unknown Author";
+		}
+
+		var tweetText = "Quote of the day - " + input.quoteText + " Author: " + input.quoteAuthor;
+
+		if (tweetText.lenght > 140) {
+			getQuote();
+		} else {
+			var tweet = tweetLink + encodeURIComponent(tweetText);
+			$('.quote').text(input.quoteText);
+			$('.author').text("Author: " + input.quoteAuthor);
+			$('.tweet').attr('href', tweet);
+		}
+	}
 }
 
-function createTweet(input) {
-	if(!input.quoteAuthor.lenght) {
-		input.quoteAuthor = "Unknown Author";
-	}
-
-	var tweetText = "Quote of the day - " + input.quoteText + " Author: " + input.quoteAuthor;
-
-	if (tweetText.lenght > 140) {
-		getQuote();
-	} else {
-		var tweet = tweetLink + encodeURIComponent(tweetText);
-		$('.quote').text(input.quoteText);
-		$('.author').text("Author: " + input.quoteAuthor);
-		$('.tweet').attr('href', tweet);
-	}
-}
 $(document).ready(function() {
 	getQuote();
 		$('.trigger').click(function(){
